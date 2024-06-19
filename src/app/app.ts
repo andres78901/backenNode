d6f56cb6b9c540ffe1cb57f483f0848be53c12e6
Andres Lopez <andres.lopez7890@gmail.com>
2024-06-18 20:35:07 -0500

    Se realiza integracion json web token a las apis

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,17 @@ import express from "express";
 import router from "../router/products.router";
 import { dataUser } from "../router/user.router";
 import morgan from "morgan";
+import { routerLogin } from "../router/login.router";
+import bodyParser from "body-parser";
 const app = express();
+
 app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.json());
-const intermedio = [router, dataUser];
+
+
+const intermedio = [router, dataUser, routerLogin];
 app.use("/api/v1", ...intermedio);
 export default app;
